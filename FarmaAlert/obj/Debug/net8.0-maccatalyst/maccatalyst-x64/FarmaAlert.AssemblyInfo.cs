1b96447ca0f3b811fc817ff4f479777bba1e74f1
Juan Cruz1103 <juanantoniocr1103@gmail.com>
2025-02-09 19:35:55 -0600

    Actualizacion de Estadisticas

--- FarmaAlert/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/FarmaAlert.AssemblyInfo.cs
+++ FarmaAlert/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/FarmaAlert.AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FarmaAlert")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9b474caa78d9cba9b5961f67e8f00af0c2235b01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c38a4636d0cc6f7905fad0c11907b8af1c6d1c6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FarmaAlert")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FarmaAlert")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.Versioning.TargetPlatformAttribute("MacCatalyst18.0")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("MacCatalyst17.5")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("MacCatalyst13.1")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
